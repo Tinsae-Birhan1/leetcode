63eab95c1a4ddec7b50de1826865a38f1140f885
Tinsae-Birhan1 <tinsaebirhan7@gmail.com>
2022-06-22 21:57:36 +0300

    Fizz Buzz problem solution

---
+++ fizz-buzz.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        return [str(i) * (i % 3 != 0 and i % 5 != 0) + "Fizz" *(i%3 == 0) + "Buzz" *( i%5 == 0)
+            for i in range (1, n+1)]
+        
+        
